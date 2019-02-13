c5afb2a4fe998f4bf599a762f15a704b71e4dd64
juan26bufalino <47535452+juan26bufalino@users.noreply.github.com>
2019-02-13 11:51:26 -0500

    Add files via upload
    Sixth Submission

---
+++ P2T1_SalesPrediction_Da'JuanAvant.py
@@ -0,0 +1,14 @@
+# Sales Prediction
+# 2/11/2019
+# CTI-110 P2T1 - Sales Prediction
+# Da'Juan Avant
+
+
+total_sales= float(input('Enter the projected sales'))
+
+# Calculate the profit as 23 percent of total sales.
+profit= total_sales * .23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
