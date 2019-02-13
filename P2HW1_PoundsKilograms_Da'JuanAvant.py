7c267f18687601d6e7edfd6c2f533eab43abae32
juan26bufalino <47535452+juan26bufalino@users.noreply.github.com>
2019-02-13 11:52:26 -0500

    Add files via upload
    Seventh Submission

---
+++ P2HW1_PoundsKilograms_Da'JuanAvant.py
@@ -0,0 +1,14 @@
+# Pounds to Kilograms Converter
+# 2/11/2019
+# CTI-110 P2T1 - Pounds to Kilograms Converter
+# Da'Juan Avant
+
+# pounds from user
+pounds= float(input('Enter number of pounds'))
+
+# kg= lb/2.2046
+kilograms= pounds/2.2046
+
+# Display total kilograms 
+print("number of kilograms", kilograms )
+
